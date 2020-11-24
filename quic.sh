4146666d49668f57701c73a7eacf2c53d19aec4f
myeongkil <clevermk7211@gmail.com>
2020-11-24 16:25:02 +0900

    added docker image build command

--- quic.sh
+++ quic.sh
@@ -3,7 +3,6 @@ rootdir="$( cd "$(dirname "$0")" ; pwd -P )"
 bdir=$rootdir/build
 sdir=$rootdir/src
 net=quic_test_net
-
 cecho(){
     RED="\033[1;31m"
     GREEN="\033[0;32m"
@@ -25,30 +24,29 @@ function quic_usage {
     cecho "GREEN" " - build     : generate dummy_certs && copy code && build docker image"
     cecho "GREEN" " - up        : QUIC server docker up"
     cecho "GREEN" " - down      : QUIC server docker down"
+    cecho "GREEN" " - docker    : build docker image"
     cecho "BLUE"  "-------------------------------------------------------------------------"
 }
 
 function quic_build {
-    mkdir -p $bdir/ssl_certs && \
-    cd $bdir/ssl_certs && \
-    openssl genrsa 2024 > server.key && \
-    openssl req -new -key server.key -subj "/C=KR" > server.csr && \
-    openssl x509 -req -days 3650 -signkey server.key < server.csr > server.crt
+    mkdir -p $bdir/ssl_certs
 
     cp $sdir/quic-client.js $bdir/
     cp $sdir/quic-server.js $bdir/
 
-    cd $rootdir && \
-    docker build . -t node-quic-example:latest
+    cd $bdir/ssl_certs && \
+    openssl genrsa 2024 > server.key && \
+    openssl req -new -key server.key -subj "/C=KR" > server.csr && \
+    openssl x509 -req -days 3650 -signkey server.key < server.csr > server.crt
 }
 
 function quic_up {
     docker network inspect $net >/dev/null 2>&1 || docker network create --driver bridge $net
-    docker-compose up -f $rootdir/docker-compose.yaml -d
+    docker-compose -f $rootdir/docker-compose.yaml up -d
 }
 
 function quic_down {
-    docker-compose down -f $rootdir/docker-compose.yaml -v
+    docker-compose -f $rootdir/docker-compose.yaml down -v
     docker network rm $net >/dev/null 2>&1
 }
 
@@ -60,13 +58,18 @@ function quic_clean {
 
 function quic_all {
     quic_clean
-    build
+    quic_build
     quic_up
+}
+
+function quic_docker {
+    cd $sdir && \
+    docker build . -t myeongkil/node-quic:latest
 }
 
 function main {
     case $1 in
-        all | build | up | down | clean)
+        all | build | up | down | clean | docker)
             cmd=quic_$1
             shift
 			$cmd $@
